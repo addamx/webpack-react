a2ce5544a45631279ff9e84d6772a405a0ce03a6
addams <addams.x@infinitytechnologies.com.cn>
2018-01-04 10:24:14 +0800

    ^init

--- src/components/common/Filmmaker.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default (props) => (
-  <div>
-    <div>{props.title}{" "}{props.rating.stars}{" "}{props.rating.average}</div>
-    <div><img src={props.images.small} alt={props.alt} /></div>
-  </div>
-)
--- src/constants/actionTypes.js
+++
@@ -1,9 +0,0 @@
-const SAVE_IN_THEATERS = 'saveInTheaters'
-const SAVE_SEARCH_RESULT = 'saveSearchResult'
-const SAVE_COMING_SOON = 'saveComingSoon'
-const SAVE_TOP_IN_ALL = 'saveTopInAll'
-const SAVE_TOP_IN_NA = 'saveTopInNA'
-const LOADING_SHOW = 'loadingShow'
-const LOADING_HIDE = 'loadingHide'
-
-export { SAVE_IN_THEATERS, SAVE_SEARCH_RESULT, SAVE_COMING_SOON, SAVE_TOP_IN_ALL, SAVE_TOP_IN_NA, LOADING_SHOW, LOADING_HIDE};
